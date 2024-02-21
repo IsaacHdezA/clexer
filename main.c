4375cd91e51d3da0212cfff37d64cfa7dca451df
IsaacHdezA <aldoisaachernandez@outlook.com>
2024-02-21 12:48:32 -0600

    [MAIN]: Token, Node and List structures working (still needs memmory management)

--- main.c
+++ main.c
@@ -6,12 +6,111 @@ void runFile(char *filename);
 void runPrompt();
 void run(char *buffer, int size);
 
+typedef struct token_t {
+  char value[BUFFER_SIZE];
+} Token;
+
+Token *createToken(char *str) {
+  int size = strlen(str);
+
+  Token *token = myMalloc(Token, 1);
+
+  memset(token, 0, sizeof(token));
+  strncpy(token->value, str, size);
+
+  return token;
+}
+
+void printToken(Token *token) {
+  printf("\"%s\"", token->value);
+}
+
+typedef struct node_t {
+  Token *token;
+  struct node_t *next;
+} Node;
+
+Node *createNode(Token *token) {
+  Node *node = myMalloc(Node, 1);
+  memset(node, 0, sizeof(node));
+
+  node->token = token;
+  node->next = NULL;
+  
+  return node;
+}
+
+void printNode(Node *node) {
+  printf("Token: ");
+  printToken(node->token);
+}
+
+typedef struct list_t {
+  Node *head;
+
+  int size;
+} List;
+
+List *createList() {
+  List *list = myMalloc(List, 1);
+
+  list->head = NULL;
+  list->size = 0;
+
+  return list;
+}
+
+void push(List *list, Token *token) {
+  if(!list->head) {
+    list->head = createNode(token);
+    list->size++;
+
+    return;
+  }
+
+  Node *aux = list->head;
+  while(aux->next) aux = aux->next;
+
+  aux->next = createNode(token);
+  list->size++;
+
+  return;
+}
+
+void printList(List *list) {
+  Node *aux = list->head;
+
+  printf("[");
+  while(aux) {
+    printNode(aux);
+    if(aux->next) printf(", ");
+    aux = aux->next;
+  }
+  printf("]\n");
+}
+
 int main(int argc, char **argv) {
-  if(argc > 2) {
-    printf("Usage: clox [script]");
-    return 64;
-  } else if (argc == 2) runFile(argv[1]);
-  else runPrompt();
+  Token *token1 = createToken("token 1"),
+        *token2 = createToken("token 2"),
+        *token3 = createToken("token 3");
+  List *list = createList();
+
+  push(list, token1);
+  push(list, token2);
+  push(list, token3);
+
+  printList(list);
+
+  free(token1);
+  free(token2);
+  free(token3);
+  free(list);
+
+  // if(argc > 2) {
+  //   printf("Usage: clox [script]");
+  //   return 64;
+  // } else if (argc == 2) runFile(argv[1]);
+  // else runPrompt();
 
   return 0;
 }

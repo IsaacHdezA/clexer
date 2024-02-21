19518a55498d156a7417023406dc0a204b003dab
IsaacHdezA <aldoisaachernandez@outlook.com>
2024-02-20 21:09:06 -0600

    [MAIN]: Added trim (rtrim, ltrim) function and substring

--- main.c
+++ main.c
@@ -2,11 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Macros
+#define isWhitespace(c) (\
+                          (c == '\t') ||\
+                          (c == '\n') ||\
+                          (c == '\v') ||\
+                          (c == '\f') ||\
+                          (c == '\r') ||\
+                          (c ==  ' ')\
+                        )
+
 // Constants
 #define BUFFER_SIZE 256
 
+int rtrim(char *str, int len);
+int ltrim(char *str, int len);
+void trim(char *dest, char *src);
+void substring(char *dest, char *src, int start, int end);
+
 void runFile(char *filename);
 void runPrompt();
+void run(char *buffer, int size);
 
 int main(int argc, char **argv) {
   if(argc > 2) {
@@ -18,7 +34,42 @@ int main(int argc, char **argv) {
   return 0;
 }
 
+int ltrim(char *str, int len) {
+  int start = 0;
+  while(isWhitespace(str[start])) start++;
+
+  return start;
+}
+
+int rtrim(char *str, int len) {
+  int end = len;
+  while(isWhitespace(str[end]) || str[end] == '\0') end--;
+
+  return end + 1;
+}
+
+
+void substring(char *dest, char *src, int start, int end) {
+  int i = 0;
+  for (i = 0; i < (end - start); i++) dest[i] = src[i + start];
+  dest[i] = '\0';
+}
+
+void trim(char *dest, char *src) {
+  if(src[0] == '\n') {
+    src[0] = '\0';
+    return;
+  }
+
+  int len   = strlen(src) - 1,
+      start = ltrim(src, len),
+      end   = rtrim(src, len);
+
+  substring(dest, src, start, end);
+}
+
 void runFile(char *filename) {
+  trim(filename, filename);
   printf("Running file \"%s\"", filename);
 
   FILE *file = fopen(filename, "r");
@@ -41,10 +92,15 @@ void runPrompt() {
   printf("Lox > ");
 
   while(fgets(buffer, BUFFER_SIZE, stdin)) {
-    run(buffer, strlen());
+    trim(buffer, buffer);
+    run(buffer, strlen(buffer));
     printf("Lox > ");
   }
 
   printf("\n");
+}
+
+void run(char *buffer, int size) {
+  printf("(%d bytes) \"%s\"\n", size, buffer);
 }
 
